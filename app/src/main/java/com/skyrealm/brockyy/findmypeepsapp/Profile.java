6d1abef18ecc5b3e4a57d000c24b4dc69fab0e8d
RAGING3K <andrew.martin.louis@gmail.com>
2015-04-11 17:13:47 -0700

    V.0.9.9.3
    Added change Username, and pending

--- app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/Profile.java
+++ app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/Profile.java
@@ -1,59 +1,146 @@
 package com.skyrealm.brockyy.findmypeepsapp;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.skyrealm.brockyy.findmypeepsapp.JSONParser;
 
-public class Profile extends ActionBarActivity {
-    private String user;
+public class Profile extends Activity implements OnClickListener{
+    String user;
+    private EditText userchange;
+    TextView usernameTextView;
+    private Button bChange;
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+    private static final String LOGIN_URL = "http://skyrealmstudio.com/ChangeUser.php";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-            View Prof = findViewById(R.id.Profileview);
-            final TextView usernameTextView = (TextView) findViewById(R.id.usernameTextView);
-
-            user = getIntent().getExtras().getString("username");
-            Prof.setOnTouchListener(new OnSwipeTouchListener(Profile.this) {
-                //calls on the swipeRight method
-                public void onSwipeRight() {
-                    Intent intent = new Intent(Profile.this, MainActivity.class);
-                    intent.putExtra("username", user);
-                    startActivity(intent);
-                }
-
-            });
-            usernameTextView.setText(user);
-        }
+        userchange = (EditText)findViewById(R.id.Newuser);
+        bChange = (Button)findViewById(R.id.Change);
+        bChange.setOnClickListener(this);
+        user = getIntent().getExtras().getString("username");
+        final View mainView = findViewById(R.id.Profileview);
+        usernameTextView = (TextView)findViewById(R.id.usernameTextView);
 
 
 
+        OnSwipeTouchListener swipeListener;
+        mainView.setOnTouchListener(new OnSwipeTouchListener(Profile.this) {
+            public void onSwipeLeft() {
+                Intent intent = new Intent(Profile.this, MainActivity.class);
+                intent.putExtra("username", user);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+        usernameTextView.setText(user);
+
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.Change:
+                new AttemptChange().execute();
+
+                // here we have used, switch case, because on login activity you may //also want to show registration button, so if the user is new ! we can go the //registration activity , other than this we could also do this without switch //case.
+            default:
+                break;
+        }
+    }
+
+    class AttemptChange extends AsyncTask<String, String, String> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Profile.this);
+            pDialog.setMessage("Attempting to change Username...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // here Check for success tag
+
+            int success;
+            String newuser = userchange.getText().toString();
+            try {
+
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("Newuser", newuser));
+                params.add(new BasicNameValuePair("username", user));
+                user = newuser;
+                Log.d("request!", "starting");
+
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "POST", params);
+
+                // checking  log for json response
+                Log.d("Registry attempt", json.toString());
+
+                // success tag for json
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("You are Registered!", json.toString());
+                    return json.getString(TAG_MESSAGE);
+                }else{
+
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        /**
+         * Once the background process is done we need to  Dismiss the progress dialog asap
+         * **/
+        protected void onPostExecute(String message) {
+
+            pDialog.dismiss();
+            if (message != null){
+
+                usernameTextView.setText(user);
+                Toast.makeText(Profile.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
