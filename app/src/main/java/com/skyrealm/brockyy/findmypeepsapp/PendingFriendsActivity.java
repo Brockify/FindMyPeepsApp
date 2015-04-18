45875d0b120b8db97e5208029d2863028e08a2e9
Brock <brdamico@csumb.edu>
2015-04-18 10:39:53 -0700

    Revert d0c9469..36f00d3
    This rolls back to commit d0c94692f60d7a67c0a478bdd659493ebc82d631.

--- app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/PendingFriendsActivity.java
+++ app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/PendingFriendsActivity.java
@@ -52,6 +52,7 @@ public class PendingFriendsActivity extends ActionBarActivity implements SwipeRe
      private String responseBody;
      private SwipeRefreshLayout swipeLayout;
      private String user;
+    public static final int LIGHT_BLUE = Color.rgb(135,206,250);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
