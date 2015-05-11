e5f71ddaf111445529d74a1cc35fda9af59fd4a5
Brock <brdamico@csumb.edu>
2015-05-11 02:20:03 -0700

    v.1.1
    Added circle bitmap crop

--- app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/MainActivity.java
+++ app/src/main/java/com/skyrealm/brockyy/findmypeepsapp/MainActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -466,16 +471,6 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         boolean isUserLoggedIn;
 
-        public void setUpDownloadImageTask(String userMarker)
-        {
-            if(user == userMarker)
-            {
-                isUserLoggedIn = true;
-            } else {
-                isUserLoggedIn = false;
-            }
-        }
-
         protected Bitmap doInBackground(String... urls) {
             String urldisplay = urls[0];
             Bitmap mIcon11 = null;
@@ -496,6 +491,7 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
             {
                 Bitmap bhalfsize = result.createScaledBitmap(result, result.getWidth()/7, result.getHeight()/7, false);
 
+               bhalfsize = getCroppedBitmap(bhalfsize);
 
                 otherUserMarker = googleMap.getMap().addMarker(new MarkerOptions()
                         .position(new LatLng(otherUserLat, otherUserLong))
@@ -505,6 +501,7 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
 
         } else {
                 Bitmap bhalfsize = result.createScaledBitmap(result, result.getWidth() / 7, result.getHeight() / 7, false);
+                bhalfsize = getCroppedBitmap(bhalfsize);
                 if (markerCounter > 0)
                 {
                     userMarker.remove();
@@ -520,5 +517,27 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
 
         }
     }
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
 }
+
 
